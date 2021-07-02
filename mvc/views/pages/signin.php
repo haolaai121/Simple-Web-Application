911d305a8cb0e6bfd0e43d301045f8bf646088a0
haolaai121 <73248804+haolaai121@users.noreply.github.com>
2021-07-02 12:21:59 +0700

    Update 2

--- mvc/views/pages/signin.php
+++ mvc/views/pages/signin.php
@@ -1,10 +1,10 @@
 <div class="main">
     <p class="sign" align="center">Sign in</p>
-    <form class="form1">
+    <form class="form1" action="./Signin/UserSignin">
       <input id="txt_signin_user"  class="un " type="text" align="center" placeholder="Username">
       <input id="txt_signin_pwn" class="pass" type="password" align="center" placeholder="Password">
 
       <a id="btn_signin_signup"class="submit" align="left" href="./Signup">Sign up</a>
       
-      <a id="btn_signin_signin"class="submit1" align="left" href="./Home">Sign in</a>
+      <button type="submit" id="btn_signin_signin"class="submit1" align="left" href="#">Sign in</button>
 
